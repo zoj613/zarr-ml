af53a47f7a25d88855c42dd6e5bc31c1bce5e37f
Zolisa Bleki <zolisa.bleki@gmail.com>
2024-06-30 12:05:14 +0200

    Expose a top-level interface and hide other modules.

---
+++ lib/zarr.ml
@@ -0,0 +1,6 @@
+module Node = Node
+module Indexing = Util.Indexing
+module ArrayMetadata = Metadata.ArrayMetadata
+module GroupMetadata = Metadata.GroupMetadata
+module Storage = Storage
+module Codecs = Codecs

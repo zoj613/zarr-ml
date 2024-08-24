b5d52b7d682e173debd91faf6b1f5cf2f9120a21
Zolisa Bleki <zolisa.bleki@gmail.com>
2024-08-24 23:42:30 +0200

    Add support for Eio-backed asynchronous I/O.
    This allows one to use the `Eio` library for asynchronous reads and
    writes to a store as an alternative to the widely used `Lwt` library.

---
+++ zarr-eio/src/deferred.ml
@@ -0,0 +1,13 @@
+type 'a t = 'a
+let return = Fun.id
+let return_unit = ()
+let iter = List.iter
+let iter_s = iter
+let fold_left = List.fold_left
+let map = List.map
+let concat_map = List.concat_map
+
+module Infix = struct
+  let (>>=) x f = f x
+  let (>>|) = (>>=)
+end
